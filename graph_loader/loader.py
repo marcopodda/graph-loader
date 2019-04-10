952e6f1c9889d6af5fce6bcad31bd57244b9beb6
Marco Podda <marco.podda@di.unipi.it>
2019-04-10 12:38:50 +0200

    first commit

--- graph_loader/loader.py
+++ graph_loader/loader.py
@@ -11,7 +11,7 @@ import numpy as np
 import sklearn
 
 # useful directories
-ROOT_DIR = Path(".")
+ROOT_DIR = Path(__file__).parent
 DATA_DIR = ROOT_DIR / "DATA"
 REPO_DIR = DATA_DIR / "REPO"
 RAW_DIR = DATA_DIR / "RAW"
